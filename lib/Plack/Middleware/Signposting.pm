5bb5d0a57396b0a0875ed46b812869908f8fd5f6
Vitali Peil <vitali.peil@uni-bielefeld.de>
2017-09-29 08:12:08 +0200

    some refactoring + new package Catmandu

--- lib/Plack/Middleware/Signposting.pm
+++ lib/Plack/Middleware/Signposting.pm
@@ -1,10 +1,22 @@
 package Plack::Middleware::Signposting;
 
-use strict;
-use warnings;
-
+use Catmandu::Sane;
 use parent 'Plack::Middleware';
+
 our $VERSION = '0.02';
+
+sub to_link_format {
+    my ($self, @signs) = @_;
+
+    my $body = join(", ", map {
+        my ($uri, $relation, $type) = @$_;
+        my $link_text = qq|<$uri>; rel="$relation"|;
+        $link_text .= qq|; type="$type"| if $type;
+        $link_text;
+    } @signs);
+
+    $body;
+}
 
 1;
 
@@ -15,5 +27,43 @@ __END__
 =head1 NAME
 
 Plack::Middleware::Signposting - a base class for Signposting implementations
+
+=begin markdown
+
+[![Build Status](https://travis-ci.org/LibreCat/Plack-Middleware-Signposting.svg?branch=master)](https://travis-ci.org/LibreCat/Plack-Middleware-Signposting)
+[![Coverage Status](https://coveralls.io/repos/github/LibreCat/Plack-Middleware-Signposting/badge.svg?branch=master)](https://coveralls.io/github/LibreCat/Plack-Middleware-Signposting?branch=master)
+
+=end markdown
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Signposting is a base class for L<<Signposting>>( https://signposting.org) protocol.
+
+=head2 METHODS
+
+=over
+
+=item to_link_format(ARRAYREF)
+
+This method produces the format for the link header. It expects an arrayref as input.
+
+=back
+
+=head1 AUTHOR
+
+Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
+
+Vitali Peil, C<< <vitali.peil at uni-bielefeld.de> >>
+
+=head1 COPYRIGHT
+
+Copyright 2017 - Vitali Peil
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
 
 =cut
--- lib/Plack/Middleware/Signposting/Catmandu.pm
+++ lib/Plack/Middleware/Signposting/Catmandu.pm
@@ -1,9 +1,8 @@
-package Plack::Middleware::Signposting::JSON;
+package Plack::Middleware::Signposting::Catmandu;
 
 use Catmandu::Sane;
 use Catmandu;
 use Catmandu::Fix;
-use JSON qw(decode_json);
 use Plack::Request;
 use Plack::Util::Accessor qw(fix);
 use Moo;
@@ -22,22 +21,26 @@ sub call {
     # only get/head requests
     return $res unless $request->method =~ m{^get|head$}i;
 
+    # match path
+    if ($request->path =~ m{publication/(\w+?)/?}) {
+        my $id = $1;
+    } else {
+        return $res;
+    }
+
     # see http://search.cpan.org/~miyagawa/Plack-1.0044/lib/Plack/Middleware.pm#RESPONSE_CALLBACK
     return $self->response_cb($res, sub {
         my $res = shift;
 
         my $content_type = Plack::Util::header_get($res->[1], 'Content-Type') || '';
         # only json responses
-        return unless $content_type =~ m{^application/json|application\/vnd\.api\+json}i;
+        #return unless $content_type =~ m{^application/json|application\/vnd\.api\+json}i;
         # ignore streaming response for now
         return unless ref $res->[2] eq 'ARRAY';
 
-        my $body = join('', @{$res->[2]});
-        my $data = decode_json($body);
-
-        if (ref $data && ref $data eq 'ARRAY') {
-            $data = $data->[0];
-        }
+        state $bag = Catmandu->store(..)->bag(..);
+        my $data = $bag->get($id);
+        return unless $data->{_id};
 
         my $fix = $self->fix ? $self->fix : 'nothing()';
         my $fixer = Catmandu::Fix->new(fixes => [$fix]);
@@ -63,6 +66,14 @@ __END__
 
 Plack::Middleware::Signposting::JSON - A Signposting implementation from JSON content
 
+=begin markdown
+
+[![Build Status](https://travis-ci.org/LibreCat/Plack-Middleware-Signposting.svg?branch=master)](https://travis-ci.org/LibreCat/Plack-Middleware-Signposting)
+[![Coverage Status](https://coveralls.io/repos/github/LibreCat/Plack-Middleware-Signposting/badge.svg?branch=master)](https://coveralls.io/github/LibreCat/Plack-Middleware-Signposting?branch=master)
+
+=end markdown
+
+
 =head1 SYNOPSIS
 
     builder {
@@ -70,5 +81,26 @@ Plack::Middleware::Signposting::JSON - A Signposting implementation from JSON co
 
        sub {200, ['Content-Type' => 'text/plain'], ['hello world']};
     };
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Signposting::JSON is a base class for Signposting(https://signposting.org) protocol.
+
+=head1 AUTHOR
+
+Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
+
+Vitali Peil, C<< <vitali.peil at uni-bielefeld.de> >>
+
+=head1 COPYRIGHT
+
+Copyright 2017 - Vitali Peil
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
 
 =cut
